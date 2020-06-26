dc03e2582535ab22f8ca2ec1cde615d621d0ff72
Sasaki Taichi <taichi_sasaki@nidek.co.jp>
2020-06-26 15:36:19 +0900

    名前追加時に重複していた場合の処理追加

--- lib/dao/name_dao.dart
+++ lib/dao/name_dao.dart
@@ -7,9 +7,23 @@ class NameDao {
   final tableName = DatabaseService.nameTableName;
 
   Future<int> create(Name name) async {
-    final db = await dbProvider.database;
-    final result = db.insert(tableName, name.toDatabaseJson(), conflictAlgorithm: ConflictAlgorithm.replace);
-    return result;
+    try {
+      final db = await dbProvider.database;
+      final result = db.insert(tableName, name.toDatabaseJson(),
+          conflictAlgorithm: ConflictAlgorithm.rollback);
+      return result;
+    
+    // 追加した名前が既にDBにあった場合の処理
+    } catch (e) {
+      final db = await dbProvider.database;
+      final List<Map<String, dynamic>> result = await db
+          .query(tableName, where: 'name_id = ?', whereArgs: [name.nameId]);
+      final List<Name> names = result.isNotEmpty
+          ? result.map((item) => Name.fromDatabaseJson(item)).toList()
+          : [];
+      // namesには常に値が1つしか入っていない想定
+      return names[0].nameId;
+    }
   }
 
   Future<List<Name>> getAll() async {
@@ -24,13 +38,16 @@ class NameDao {
   Future<int> update(Name name) async {
     final db = await dbProvider.database;
     final result = await db.update(tableName, name.toDatabaseJson(),
-        where: 'name_id = ?', whereArgs: [name.nameId], conflictAlgorithm: ConflictAlgorithm.replace);
+        where: 'name_id = ?',
+        whereArgs: [name.nameId],
+        conflictAlgorithm: ConflictAlgorithm.replace);
     return result;
   }
 
   Future<int> delete(int id) async {
     final db = await dbProvider.database;
-    final result = await db.delete(tableName, where: 'name_id = ?', whereArgs: [id]);
+    final result =
+        await db.delete(tableName, where: 'name_id = ?', whereArgs: [id]);
     return result;
   }
 
